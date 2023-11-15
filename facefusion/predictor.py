339338d1f0b74afd94120cedb6f7bdcd56db2191
Kristian Ye <jye2020@yahoo.com>
2023-11-15 23:33:06 +0200

    Removed NSFW

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -30,29 +30,20 @@ def clear_predictor() -> None:
 	PREDICTOR = None
 
 
-def predict_stream(frame : Frame) -> bool:
-	global STREAM_COUNTER
-
-	STREAM_COUNTER = STREAM_COUNTER + 1
-	if STREAM_COUNTER % FRAME_INTERVAL == 0:
-		return predict_frame(frame)
-	return False
+def predict_stream(frame: Frame) -> bool:
+    return False
 
 
-def predict_frame(frame : Frame) -> bool:
-	image = Image.fromarray(frame)
-	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-	views = numpy.expand_dims(image, axis = 0)
-	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+def predict_frame(frame: Frame) -> bool:
+    return False
 
 
-@lru_cache(maxsize = None)
-def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+@lru_cache(maxsize=None)
+def predict_image(image_path: str) -> bool:
+    return False
 
 
-@lru_cache(maxsize = None)
-def predict_video(video_path : str) -> bool:
-	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = FRAME_INTERVAL)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+@lru_cache(maxsize=None)
+def predict_video(video_path: str) -> bool:
+    return False
+	
